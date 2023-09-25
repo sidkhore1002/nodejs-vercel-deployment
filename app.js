41af6b6ba6c6ad3881737bdeb915a8cfe6006e7f
siddharth khore <35920894+sidkhore1002@users.noreply.github.com>
2023-09-25 19:41:50 +0530

    first
    first

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+ 
+console.log("sidddd");
+
+app.listen(4500, console.log(`Server started on port 4500`));
+
+app.get('/', (req, res) => {
+    res.send('Vercel Deployment Done...')
+    res.end()
+})    
